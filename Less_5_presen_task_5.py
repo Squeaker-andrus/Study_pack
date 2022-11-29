a02c46754a55f15919950dfa25d0e4e0468d8579
Squeaker-andrus <andrussvarog1024@gmail.com>
2022-11-29 04:51:14 +0300

    v0.1 Занятие 5 Задание 5 Презентация

--- Less_5_presen_task_5.py
+++ Less_5_presen_task_5.py
@@ -3,6 +3,7 @@ list_of_num = []
 for i in text_num:
     if i.isdigit() or (i == '-' and text_num[0] == '-') or (i == '.' and text_num[]
         list_of_num.append(i)
+
     else:
         continue
     # Брррр, нет времени позже разберусь подробнее
