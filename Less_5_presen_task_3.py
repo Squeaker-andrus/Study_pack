a049ac83f8f27119a356ec261078aebc85a678ec
Squeaker-andrus <andrussvarog1024@gmail.com>
2022-11-29 04:16:28 +0300

    v0.1 Занятие 5 Задание 2 Презентация

---
+++ Less_5_presen_task_3.py
@@ -0,0 +1,6 @@
+spisok_slov = ('Олово', 'Комок', 'Ара', 'Клюшка', 'Польша', 'Дорога', 'Увертюра', 'Топот')
+res = []
+def check_polindrom(spisok_slov):
+
+filter(lambda spisok_slov: spisok_slov if list[str(spisok_slov[::1]).isalpha()] == list[str(spisok_slov[::-1]).isalpha()] else continue, ('Олово', 'Комок', 'Ара', 'Клюшка', 'Польша', 'Дорога', 'Увертюра', 'Топот'))
+# Запутался
