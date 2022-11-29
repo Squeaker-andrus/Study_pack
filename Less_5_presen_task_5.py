8f489fa9d979310b7e49e28389dddf1f8270f16a
Squeaker-andrus <andrussvarog1024@gmail.com>
2022-11-29 04:50:50 +0300

    v0.1 Занятие 5 Задание 3 Презентация

---
+++ Less_5_presen_task_5.py
@@ -0,0 +1,8 @@
+text_num = input('Введите вашу цифру ')
+list_of_num = []
+for i in text_num:
+    if i.isdigit() or (i == '-' and text_num[0] == '-') or (i == '.' and text_num[]
+        list_of_num.append(i)
+    else:
+        continue
+    # Брррр, нет времени позже разберусь подробнее
