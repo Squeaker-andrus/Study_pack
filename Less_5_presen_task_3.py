50cadd5e1bbcc84bf5d44771c004c9cdeb383633
Squeaker-andrus <andrussvarog1024@gmail.com>
2022-12-04 13:26:39 +0300

    v0.2 Занятие 5 Задание 5 Презентация

--- Less_5_presen_task_3.py
+++ Less_5_presen_task_3.py
@@ -1,9 +1,3 @@
 spisok_slov = ('Олово', 'Комок', 'Ара', 'Клюшка', 'Польша', 'Дорога', 'Увертюра', 'Топот')
-res = []
-def check_polindrom(spisok_slov : tuple -> tuple):
-    for i in spisok_slov:
-        if list[str().isalpha()].reverse() == list[str().isalpha()]:
-            return i
 
-filter(lambda spisok_slov: spisok_slov if list[str(spisok_slov[::1]).isalpha()] == list[str(spisok_slov[::-1]).isalpha()] else continue, ('Олово', 'Комок', 'Ара', 'Клюшка', 'Польша', 'Дорога', 'Увертюра', 'Топот'))
-# Запутался
+print(list(filter(lambda word: word.upper() == word[::-1].upper(), spisok_slov)))
