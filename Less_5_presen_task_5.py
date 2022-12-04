f173737ab3512176aef589a3302b3a559d3f4928
Squeaker-andrus <andrussvarog1024@gmail.com>
2022-12-04 15:25:02 +0300

    v0.2 Занятие 5 Задание 5 Презентация

--- Less_5_presen_task_5.py
+++ Less_5_presen_task_5.py
@@ -1,9 +1,24 @@
 text_num = input('Введите вашу цифру ')
-list_of_num = []
-for i in text_num:
-    if i.isdigit() or (i == '-' and text_num[0] == '-') or (i == '.' and text_num[]
-        list_of_num.append(i)
-
+def str_in_num(text_num):
+    list_of_num = str()
+    for i in text_num:
+            if i.isdigit() or (i == '-' and text_num[0] == '-') or i == '.':
+                list_of_num += i
+            else:
+                return print(f'{text_num} - некорректное число')
+    if sum(map(lambda i: i == '.', list_of_num)) >= 2:
+        return print(f'{text_num} - Некорректное число')
     else:
-        continue
-    # Брррр, нет времени позже разберусь подробнее
+        int_num = str()
+        float_num = str()
+        for i in list_of_num:
+            if i.isdigit() or (i == '-' and list_of_num[0] == '-') or i != '.':
+                int_num += i
+            elif list_of_num[0] == '.' or (list_of_num[0] == '-' and list_of_num[1] == '.'):
+                float_num += '0' + i
+            else:
+                float_num += i
+        return float(float_num) if int_num == () else int(int_num)
+print(type(str_in_num(text_num)))
+print(str_in_num(text_num))
+# Хмм, скипает точку и все выводит в int... думаем...
